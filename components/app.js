5db6572cdc1c0bf016c0bd65bc97bbfe5ae0e1e9
Cherie <cherietsukayama@gmail.com>
2020-03-20 19:58:04 +0000

    updated the script tag pointing to the jquery file. Added get grades method to the class App and the start method to the Class app.

--- components/app.js
+++ components/app.js
@@ -1,12 +1,26 @@
 class App {
-    handleGetGradesError(error){
+    constructor(){
+        var getGradesError = this.handleGetGradesError.bind(this);
+        var gGetGradesSuccess = this.handelGetGradesSuccess.bind(this);
+    }
+    handleGetGradesError(error) {
         console.error(error);
     }
     handelGetGradesSuccess(grades) {
         console.log(grades);
     }
-    constructor(){
-        var getGradesError = this.handleGetGradesError.bind(this);
-        var gGetGradesSuccess = this.handelGetGradesSuccess.bind(this);
+    getGrades(){
+        $.ajax({
+            method: "GET",
+            url: "https://sgt.lfzprototypes.com/api/grades",
+            header: {
+                "X-Access-Token": "C6nLlY8h"
+            },
+            success: this.getGradesSuccess(),
+            error: this.getGradesError()
+        })
+    }
+    start(){
+        this.getGrades;
     }
 }
