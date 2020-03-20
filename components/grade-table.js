17196dd9740f36aded833326974aa2c02b389f79
Cherie <cherietsukayama@gmail.com>
2020-03-20 22:40:41 +0000

    dynamically added table rows and table data to the table body on the html by using the grades data that is being passed through from the ajax call in the app.js file.

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,6 +3,21 @@ class GradeTable {
         this.tableEl = tableElement;
     }
     updateGrades(grades){
+        var tbody = this.tableEl.querySelector('tbody');
+        for(var i = 0; i < grades.length; i++){
+            var row = document.createElement('tr');
+            var name = document.createElement('td');
+            name.textContent = grades[i].name;
+            var course = document.createElement('td');
+            course.textContent = grades[i].course;
+            var grade = document.createElement('td');
+            grade.textContent = grades[i].grade;
+            row.appendChild(name);
+            row.appendChild(course);
+            row.appendChild(grade);
+            tbody.appendChild(row);
+        }
+
         console.log(grades);
     }
 }
