05c60f597f716a52580f0072c8608beaeea160d7
Cherie <cherietsukayama@gmail.com>
2020-03-20 23:50:50 +0000

    modified the instantiation of the new App to recieve a second argument. The second argument is the variable that instantiated the new PageHeader

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var headerEl = document.querySelector("header");
 var tableElement = document.querySelector("table");
 var pageHeader = new PageHeader(headerEl);
 var  table = new GradeTable(tableElement);
-var app = new App(table);
+var app = new App(table, pageHeader);
 app.start();
