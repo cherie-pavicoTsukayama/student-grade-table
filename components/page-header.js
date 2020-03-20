f643cec58e3aec589a634cfdf129045a9bc02fed
Cherie <cherietsukayama@gmail.com>
2020-03-20 23:44:19 +0000

    added a class named PageHeader to pageHeader.js. Created a constructor that takes one argument and assigns it to a property in the object. Created a method called updateAverage that takes one argument.

---
+++ components/page-header.js
@@ -0,0 +1,8 @@
+class PageHeader {
+    constructor(headerElement){
+        this.headerElement = headerElement;
+    }
+    updateAverage(newAverage){
+        console.log("Result from the updateAverage method:", newAverage);
+    }
+}
