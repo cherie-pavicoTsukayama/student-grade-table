1fb60e3d8762ea05900fc3ee09ae6aedab3d4dfc
Cherie <cherietsukayama@gmail.com>
2020-03-21 18:43:56 +0000

    on the GradeForm class in the constructor I bound the method handleSubmit to the constructor and assigned it to a property this.handleSubmit. I added an event listener to listen for the event 'subtmit' and whon the formElement and when it hears a 'submit' it will execute the handleSubmit method that is bound to the object.

--- components/grade-form.js
+++ components/grade-form.js
@@ -1,6 +1,8 @@
 class GradeForm {
     constructor (formElement) {
         this.formElement = formElement;
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.formElement.addEventListner('submit', this.handleSubmit);
     }
     onSubmit(createGrade){
         this.createGrade = createGrade;
