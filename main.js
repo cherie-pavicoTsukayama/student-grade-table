2c5b508fe8ff72b7b3ac4587d537dcc1c7a8929f
Cherie <cherietsukayama@gmail.com>
2020-03-21 18:49:02 +0000

    on the main js i used the querySelector to get the element form and assigned it to variable formEl. I insantiated the GradeForm class using the var formEl as a parameter and assigned it to the variable gradeForm.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var headerEl = document.querySelector("header");
 var tableElement = document.querySelector("table");
 var pageHeader = new PageHeader(headerEl);
-var  table = new GradeTable(tableElement);
+var table = new GradeTable(tableElement);
 var app = new App(table, pageHeader);
+var formEl = document.querySelector("form");
+var gradeForm = new GradeForm(formEl);
 app.start();
