7cce35d1ae240078a60beb28723c7b3804fc6d85
Cherie <cherietsukayama@gmail.com>
2020-03-21 23:47:37 +0000

    In the main.js i queried the DOM for the p element and assigned it to a variable and passed that variable in as a second argument for the GradeTable instantiation

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var headerEl = document.querySelector("header");
 var tableElement = document.querySelector("table");
 var pageHeader = new PageHeader(headerEl);
-var table = new GradeTable(tableElement);
+var table = new GradeTable(tableElement, pElement);
 var formEl = document.querySelector("form");
 var gradeForm = new GradeForm(formEl);
 var app = new App(table, pageHeader, gradeForm);
+var pElement = document.querySelector('p');
 
 app.start();
