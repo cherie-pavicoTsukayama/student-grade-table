c9838c017dee1b32145b6c38383ba6c2b4e96a1e
Cherie <cherietsukayama@gmail.com>
2020-03-21 23:27:26 +0000

    On the grade-table.js in the GradeTable class I created a new method called renderGradeRow that takes two parameters, one that is data the other is deleteGrade which is a function. renderGradeRow creates a new tr and 4 tds. The values of the data name, course, and grade are stored in the 3 tds created. There is another td created that holds the deleteBotton element. The delete button has an event listener on it that listens for a click and passes the data.id to the deleteGrade method of the this object which is bound to the App class. The tds are all appened to a tr and the function returns the tr created

--- components/grade-table.js
+++ components/grade-table.js
@@ -22,4 +22,25 @@ class GradeTable {
     onDeleteclick(deleteGrade){
         this.deleteGrade = deleteGrade;
     }
+    renderGradeRow(data, deleteGrade){
+        var row = document.createElement('tr');
+        var name = document.createElement('td');
+        name.textContent = data.name;
+        var course = document.createElement('td');
+        course.textContent = data.course;
+        var grade = document.createElement('td');
+        grade.textContent = data.grade;
+        var deleteButton = document.createElement('button');
+        deleteButton.setAttribute('class', "btn-danger");
+        deleteButton.addEventListener('click', this.deleteGrade(data.id));
+        var deleteTd = document.createElement('td');
+        deleteTd.textContent = deleteButton;
+        row.appendChild(name);
+        row.appendChild(course);
+        row.appendChild(grade);
+        row.appendChild(deleteTd);
+        return row;
+
+
+    }
 }
