1295dbea55d365e89f531de7dd496db8d58dc80c
Cherie <cherietsukayama@gmail.com>
2020-03-21 23:38:18 +0000

    Modified the updateGrades in the GradeTable class. It now uses the renderGradeRow method of this object to gather the data, create the tds and append it to the row then it just appends the return value of the function that is being assgined to a var row and appends it to the tbody.

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,21 +1,12 @@
 class GradeTable {
-    constructor (tableElement){
+    constructor(tableElement){
         this.tableEl = tableElement;
     }
     updateGrades(grades){
         var tbody = this.tableEl.querySelector('tbody');
         tbody.innerHTML = "";
         for(var i = 0; i < grades.length; i++){
-            var row = document.createElement('tr');
-            var name = document.createElement('td');
-            name.textContent = grades[i].name;
-            var course = document.createElement('td');
-            course.textContent = grades[i].course;
-            var grade = document.createElement('td');
-            grade.textContent = grades[i].grade;
-            row.appendChild(name);
-            row.appendChild(course);
-            row.appendChild(grade);
+            var row = this.renderGradeRow();
             tbody.appendChild(row);
         }
     }
